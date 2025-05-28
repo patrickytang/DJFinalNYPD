a739f4c2628fb16c24e62b72563c102abae9c40a
patrickytang <patrick.tang26@trinityschoolnyc.org>
2025-05-28 08:51:08 -0400

    ansdf

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
+# flask --app data_server run
 from flask import Flask
 from flask import request
 from flask import render_template
 import json
+
+
 app = Flask(__name__, static_url_path='', static_folder='static')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+
+@app.route('/about')
+def year():
+    return render_template('about.html')
+
+@app.route('/borough')
+def boro():
+    return render_template('micro.html')
+app.run(debug=True)
